07b4abac1b963e723b41b17cbfe04c250db85c4a
Habibur Rahaman <shamolrahaman@gmail.com>
2026-03-13 02:41:01 +0600

    Fix latest job circular filtering and API list parsing

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,9 +1,12 @@
 class ApiUrls {
+  static const token =
+      "SDafHV5s5Jiv0V7iMV2eGphWOeXDZ8iFJUhgmrY3BK787qMS0h0xKtJOsBHPa6KR";
 
-  static const base = "https://ebdresults.com/api";
+  static const base = "https://ebdresults.com/api/v1";
 
-  static const jobs = "$base/jobs";
-  static const news = "$base/news";
-  static const results = "$base/results";
-
+  static const posts = "$base/posts";
+  static const categories = "$base/categories";
+  static const tags = "$base/tags";
+  static const pages = "$base/pages";
+  static const comments = "$base/comments";
 }
